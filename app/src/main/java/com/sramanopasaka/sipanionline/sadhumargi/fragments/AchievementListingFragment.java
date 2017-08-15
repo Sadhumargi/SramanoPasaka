a4f6acc4bba3956e6fb525c0789af390d0ad7b6c
pranavsmb <pranavjaydev@gmail.com>
2017-08-15 16:07:13 +0530

    Api integrated for address

---
+++ app/src/main/java/com/sramanopasaka/sipanionline/sadhumargi/fragments/AchievementListingFragment.java
@@ -0,0 +1,189 @@
+package com.sramanopasaka.sipanionline.sadhumargi.fragments;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sramanopasaka.sipanionline.sadhumargi.R;
+import com.sramanopasaka.sipanionline.sadhumargi.adapters.AchievementListAdapter;
+import com.sramanopasaka.sipanionline.sadhumargi.adapters.AddressListAdapter;
+import com.sramanopasaka.sipanionline.sadhumargi.cms.response.AddressListResponse;
+import com.sramanopasaka.sipanionline.sadhumargi.cms.response.DeleteAddressResponse;
+import com.sramanopasaka.sipanionline.sadhumargi.cms.response.GUIResponse;
+import com.sramanopasaka.sipanionline.sadhumargi.cms.task.RequestProcessor;
+import com.sramanopasaka.sipanionline.sadhumargi.helpers.OfflineData;
+import com.sramanopasaka.sipanionline.sadhumargi.listener.GUICallback;
+import com.sramanopasaka.sipanionline.sadhumargi.model.Achievements;
+import com.sramanopasaka.sipanionline.sadhumargi.model.Address;
+import com.sramanopasaka.sipanionline.sadhumargi.model.LoginModel;
+import com.sramanopasaka.sipanionline.sadhumargi.utils.DialogueUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Name    :   pranavjdev
+ * Date   : 8/15/17
+ * Email : pranavjaydev@gmail.com
+ */
+
+public class AchievementListingFragment extends BaseFragment implements GUICallback,AchievementListAdapter.EditDeleteActionListener {
+
+    private View view = null;
+
+    @Bind(R.id.achievementRecycler)
+    RecyclerView recyclerView;
+
+    @Bind(R.id.txt_no_address)
+    TextView txtNoAddress;
+
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
+
+    public static AchievementListingFragment newInstance() {
+        return new AchievementListingFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_achievements, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadAddressList();
+    }
+
+    private void loadAddressList(){
+        LoginModel loginResponse = OfflineData.getLoginData();
+        if (loginResponse != null) {
+
+
+            RequestProcessor requestProcessor = new RequestProcessor(AchievementListingFragment.this);
+            requestProcessor.getAddressList(loginResponse.getId(), loginResponse.getAppToken());
+        }
+    }
+
+    private void showAddressList(List<Achievements> list) {
+        if (list != null && list.size() != 0) {
+            txtNoAddress.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+            layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(layoutManager);
+            AchievementListAdapter adapter = new AchievementListAdapter(getActivity(), list, this);
+            recyclerView.setAdapter(adapter);
+        } else {
+            AchievementListAdapter adapter = new AchievementListAdapter(getActivity(), null, this);
+            recyclerView.setAdapter(adapter);
+            txtNoAddress.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+
+    @Override
+    public void onRequestProcessed(GUIResponse guiResponse, GUICallback.RequestStatus requestStatus) {
+        hideLoadingDialog();
+        if (guiResponse != null) {
+            if (requestStatus.equals(GUICallback.RequestStatus.SUCCESS)) {
+                if (guiResponse instanceof AddressListResponse) {
+                    AddressListResponse response = (AddressListResponse) guiResponse;
+                    if (response != null) {
+                        if (!TextUtils.isEmpty(response.getStatus()) && response.getStatus().equalsIgnoreCase("success")) {
+
+                            if (response.getData() != null && response.getData().size() > 0) {
+                                //showAddressList(response.getData());
+                            } else {
+                                txtNoAddress.setVisibility(View.VISIBLE);
+                                recyclerView.setVisibility(View.GONE);
+                            }
+
+                            Log.e("----", "success");
+                        } else {
+                            if (!TextUtils.isEmpty(response.getMessage())) {
+                                Toast.makeText(getActivity(), response.getMessage(), Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getActivity(), "some thing went wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }else if (guiResponse instanceof DeleteAddressResponse) {
+                    DeleteAddressResponse response = (DeleteAddressResponse) guiResponse;
+                    if (response != null) {
+                        if (!TextUtils.isEmpty(response.getStatus()) && response.getStatus().equalsIgnoreCase("success")) {
+
+                            if (!TextUtils.isEmpty(response.getMessage())) {
+                                Toast.makeText(getActivity(), response.getMessage(), Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getActivity(), "Address removed", Toast.LENGTH_SHORT).show();
+                            }
+
+                            loadAddressList();
+
+                            Log.e("----", "success");
+                        } else {
+                            if (!TextUtils.isEmpty(response.getMessage())) {
+                                Toast.makeText(getActivity(), response.getMessage(), Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getActivity(), "some thing went wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void edit(Achievements address) {
+
+    }
+
+    @Override
+    public void delete(Achievements address) {
+        DialogueUtils.showDialogOKCancel(getActivity(), "Delete Address", "Are you sure to delete this address?", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                LoginModel loginResponse = OfflineData.getLoginData();
+                if (loginResponse != null) {
+
+
+                    RequestProcessor requestProcessor = new RequestProcessor(AchievementListingFragment.this);
+                    //requestProcessor.removeAddress(loginResponse.getId(), loginResponse.getAppToken(),address);
+                }
+
+                dialogInterface.dismiss();
+            }
+        }, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+    }
+}
