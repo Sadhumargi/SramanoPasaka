4301b4800d188e1aec5d019ea7041ebfc93feb51
pranavsmb <pranavjaydev@gmail.com>
2017-08-21 10:37:20 +0530

    Api integrated for Sankh and dharmik

---
+++ app/src/main/java/com/sramanopasaka/sipanionline/sadhumargi/model/Hobby.java
@@ -0,0 +1,373 @@
+package com.sramanopasaka.sipanionline.sadhumargi.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Hobby{
+
+	@SerializedName("rel_gochary_service")
+	private String relGocharyService;
+
+	@SerializedName("rel_shivir_management")
+	private String relShivirManagement;
+
+	@SerializedName("org_migration_prg")
+	private String orgMigrationPrg;
+
+	@SerializedName("social_education_service")
+	private String socialEducationService;
+
+	@SerializedName("rel_swadhyai_service")
+	private String relSwadhyaiService;
+
+	@SerializedName("org_fund_raise")
+	private String orgFundRaise;
+
+	@SerializedName("rel_medical_service")
+	private String relMedicalService;
+
+	@SerializedName("org_comp_operator")
+	private String orgCompOperator;
+
+	@SerializedName("rel_vihar_service")
+	private String relViharService;
+
+	@SerializedName("rel_teaching")
+	private String relTeaching;
+
+	@SerializedName("rel_kar_sewa")
+	private String relKarSewa;
+
+	@SerializedName("org_oration_trainer")
+	private String orgOrationTrainer;
+
+	@SerializedName("social_speech")
+	private String socialSpeech;
+
+	@SerializedName("org_project_operation")
+	private String orgProjectOperation;
+
+	@SerializedName("social_web_handling")
+	private String socialWebHandling;
+
+	@SerializedName("social_water_kiosk_service")
+	private String socialWaterKioskService;
+
+	@SerializedName("rel_branch")
+	private String relBranch;
+
+	@SerializedName("id")
+	private String id;
+
+	@SerializedName("rel_writeup")
+	private String relWriteup;
+
+	@SerializedName("member_id")
+	private String memberId;
+
+	@SerializedName("social_veg_publicity")
+	private String socialVegPublicity;
+
+	@SerializedName("org_planning")
+	private String orgPlanning;
+
+	@SerializedName("social_medical_service")
+	private String socialMedicalService;
+
+	@SerializedName("rel_self_learning")
+	private String relSelfLearning;
+
+	@SerializedName("social_drug_rehab")
+	private String socialDrugRehab;
+
+	@SerializedName("org_management_phones")
+	private String orgManagementPhones;
+
+	@SerializedName("rel_jap_tap_cordination")
+	private String relJapTapCordination;
+
+	@SerializedName("social_human_service")
+	private String socialHumanService;
+
+	@SerializedName("rel_drawing")
+	private String relDrawing;
+
+	@SerializedName("social_lit_service")
+	private String socialLitService;
+
+	public void setRelGocharyService(String relGocharyService){
+		this.relGocharyService = relGocharyService;
+	}
+
+	public String getRelGocharyService(){
+		return relGocharyService;
+	}
+
+	public void setRelShivirManagement(String relShivirManagement){
+		this.relShivirManagement = relShivirManagement;
+	}
+
+	public String getRelShivirManagement(){
+		return relShivirManagement;
+	}
+
+	public void setOrgMigrationPrg(String orgMigrationPrg){
+		this.orgMigrationPrg = orgMigrationPrg;
+	}
+
+	public String getOrgMigrationPrg(){
+		return orgMigrationPrg;
+	}
+
+	public void setSocialEducationService(String socialEducationService){
+		this.socialEducationService = socialEducationService;
+	}
+
+	public String getSocialEducationService(){
+		return socialEducationService;
+	}
+
+	public void setRelSwadhyaiService(String relSwadhyaiService){
+		this.relSwadhyaiService = relSwadhyaiService;
+	}
+
+	public String getRelSwadhyaiService(){
+		return relSwadhyaiService;
+	}
+
+	public void setOrgFundRaise(String orgFundRaise){
+		this.orgFundRaise = orgFundRaise;
+	}
+
+	public String getOrgFundRaise(){
+		return orgFundRaise;
+	}
+
+	public void setRelMedicalService(String relMedicalService){
+		this.relMedicalService = relMedicalService;
+	}
+
+	public String getRelMedicalService(){
+		return relMedicalService;
+	}
+
+	public void setOrgCompOperator(String orgCompOperator){
+		this.orgCompOperator = orgCompOperator;
+	}
+
+	public String getOrgCompOperator(){
+		return orgCompOperator;
+	}
+
+	public void setRelViharService(String relViharService){
+		this.relViharService = relViharService;
+	}
+
+	public String getRelViharService(){
+		return relViharService;
+	}
+
+	public void setRelTeaching(String relTeaching){
+		this.relTeaching = relTeaching;
+	}
+
+	public String getRelTeaching(){
+		return relTeaching;
+	}
+
+	public void setRelKarSewa(String relKarSewa){
+		this.relKarSewa = relKarSewa;
+	}
+
+	public String getRelKarSewa(){
+		return relKarSewa;
+	}
+
+	public void setOrgOrationTrainer(String orgOrationTrainer){
+		this.orgOrationTrainer = orgOrationTrainer;
+	}
+
+	public String getOrgOrationTrainer(){
+		return orgOrationTrainer;
+	}
+
+	public void setSocialSpeech(String socialSpeech){
+		this.socialSpeech = socialSpeech;
+	}
+
+	public String getSocialSpeech(){
+		return socialSpeech;
+	}
+
+	public void setOrgProjectOperation(String orgProjectOperation){
+		this.orgProjectOperation = orgProjectOperation;
+	}
+
+	public String getOrgProjectOperation(){
+		return orgProjectOperation;
+	}
+
+	public void setSocialWebHandling(String socialWebHandling){
+		this.socialWebHandling = socialWebHandling;
+	}
+
+	public String getSocialWebHandling(){
+		return socialWebHandling;
+	}
+
+	public void setSocialWaterKioskService(String socialWaterKioskService){
+		this.socialWaterKioskService = socialWaterKioskService;
+	}
+
+	public String getSocialWaterKioskService(){
+		return socialWaterKioskService;
+	}
+
+	public void setRelBranch(String relBranch){
+		this.relBranch = relBranch;
+	}
+
+	public String getRelBranch(){
+		return relBranch;
+	}
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public void setRelWriteup(String relWriteup){
+		this.relWriteup = relWriteup;
+	}
+
+	public String getRelWriteup(){
+		return relWriteup;
+	}
+
+	public void setMemberId(String memberId){
+		this.memberId = memberId;
+	}
+
+	public String getMemberId(){
+		return memberId;
+	}
+
+	public void setSocialVegPublicity(String socialVegPublicity){
+		this.socialVegPublicity = socialVegPublicity;
+	}
+
+	public String getSocialVegPublicity(){
+		return socialVegPublicity;
+	}
+
+	public void setOrgPlanning(String orgPlanning){
+		this.orgPlanning = orgPlanning;
+	}
+
+	public String getOrgPlanning(){
+		return orgPlanning;
+	}
+
+	public void setSocialMedicalService(String socialMedicalService){
+		this.socialMedicalService = socialMedicalService;
+	}
+
+	public String getSocialMedicalService(){
+		return socialMedicalService;
+	}
+
+	public void setRelSelfLearning(String relSelfLearning){
+		this.relSelfLearning = relSelfLearning;
+	}
+
+	public String getRelSelfLearning(){
+		return relSelfLearning;
+	}
+
+	public void setSocialDrugRehab(String socialDrugRehab){
+		this.socialDrugRehab = socialDrugRehab;
+	}
+
+	public String getSocialDrugRehab(){
+		return socialDrugRehab;
+	}
+
+	public void setOrgManagementPhones(String orgManagementPhones){
+		this.orgManagementPhones = orgManagementPhones;
+	}
+
+	public String getOrgManagementPhones(){
+		return orgManagementPhones;
+	}
+
+	public void setRelJapTapCordination(String relJapTapCordination){
+		this.relJapTapCordination = relJapTapCordination;
+	}
+
+	public String getRelJapTapCordination(){
+		return relJapTapCordination;
+	}
+
+	public void setSocialHumanService(String socialHumanService){
+		this.socialHumanService = socialHumanService;
+	}
+
+	public String getSocialHumanService(){
+		return socialHumanService;
+	}
+
+	public void setRelDrawing(String relDrawing){
+		this.relDrawing = relDrawing;
+	}
+
+	public String getRelDrawing(){
+		return relDrawing;
+	}
+
+	public void setSocialLitService(String socialLitService){
+		this.socialLitService = socialLitService;
+	}
+
+	public String getSocialLitService(){
+		return socialLitService;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Hobby{" + 
+			"rel_gochary_service = '" + relGocharyService + '\'' + 
+			",rel_shivir_management = '" + relShivirManagement + '\'' + 
+			",org_migration_prg = '" + orgMigrationPrg + '\'' + 
+			",social_education_service = '" + socialEducationService + '\'' + 
+			",rel_swadhyai_service = '" + relSwadhyaiService + '\'' + 
+			",org_fund_raise = '" + orgFundRaise + '\'' + 
+			",rel_medical_service = '" + relMedicalService + '\'' + 
+			",org_comp_operator = '" + orgCompOperator + '\'' + 
+			",rel_vihar_service = '" + relViharService + '\'' + 
+			",rel_teaching = '" + relTeaching + '\'' + 
+			",rel_kar_sewa = '" + relKarSewa + '\'' + 
+			",org_oration_trainer = '" + orgOrationTrainer + '\'' + 
+			",social_speech = '" + socialSpeech + '\'' + 
+			",org_project_operation = '" + orgProjectOperation + '\'' + 
+			",social_web_handling = '" + socialWebHandling + '\'' + 
+			",social_water_kiosk_service = '" + socialWaterKioskService + '\'' + 
+			",rel_branch = '" + relBranch + '\'' + 
+			",id = '" + id + '\'' + 
+			",rel_writeup = '" + relWriteup + '\'' + 
+			",member_id = '" + memberId + '\'' + 
+			",social_veg_publicity = '" + socialVegPublicity + '\'' + 
+			",org_planning = '" + orgPlanning + '\'' + 
+			",social_medical_service = '" + socialMedicalService + '\'' + 
+			",rel_self_learning = '" + relSelfLearning + '\'' + 
+			",social_drug_rehab = '" + socialDrugRehab + '\'' + 
+			",org_management_phones = '" + orgManagementPhones + '\'' + 
+			",rel_jap_tap_cordination = '" + relJapTapCordination + '\'' + 
+			",social_human_service = '" + socialHumanService + '\'' + 
+			",rel_drawing = '" + relDrawing + '\'' + 
+			",social_lit_service = '" + socialLitService + '\'' + 
+			"}";
+		}
+}
